b9fbf4e2143347284121a28d5603652754b6baff
vembiah-1 <haran.vembiah@mt.com>
2020-07-12 13:29:31 +0530

    updated few conceptss

--- Data_structures/dictionaries.py
+++ Data_structures/dictionaries.py
@@ -28,22 +28,32 @@ for key, values in d.items():
     for value in values:
         d_list.append([key, value])
 
-d1 = {12:{'Name': 'John', 'Age': '27', 'Sex': 'Male'}}
-d2 = {13:{'Name': 'John', 'Age': '27', 'Sex': 'Male'}}
+d1 = {12:{'Name': 'John12', 'Age': '27', 'Sex': 'Male'}}
+d2 = {13:{'Name': 'John13', 'Age': '27', 'Sex': 'Male'}}
 d3 = {14:{'Name': 'John', 'Age': '27', 'Sex': 'Male'}}
 d4 = {15:{'Name': 'John', 'Age': '27', 'Sex': 'Male'}}
 
 print(d3)
 print("---------------")
 
-dic = defaultdict(list)
-dic['123'].append(d1) #Add the first dic inside the key '123'
-dic['123'].append(d2) #Add the second dic inside the key '123'
-dic['124'].append(d3)
-dic['124'].append(d4)
-print(dic['123'][0])
-print(dic['123'][1])
+dic = defaultdict(dict)
+dic['123'].update(d1) #Add the first dic inside the key '123'
+dic['123'].update(d2) #Add the second dic inside the key '123'
+dic['124'].update(d3)
+dic['124'].update(d4)
+# print(dic['123'][0])
+# print(dic['123'][1])
 print(dic)
+for x in dic:
+    print(x)
+for x in dic['123']:
+    print(f"Value of {x} is {dic['123'][x]}")
+    for k,v in dic['123'][x].items():
+        print(f"value of key is {k}")
+        print(f"value of values is {v}")
+    # print(y)
+    # print(y)
+
 print("-----------------")
 # print(dic['123']['12']['Name'])
 # for ids, backl in dic:
